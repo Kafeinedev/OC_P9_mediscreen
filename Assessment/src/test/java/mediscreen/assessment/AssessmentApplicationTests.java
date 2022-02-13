4897728fd94e5bad60e7cfe5250050b794792604
Kafeinedev <kafeinedev@protonmail.com>
2022-02-13 08:15:29 +0100

    Adding missing files

---
+++ Assessment/src/test/java/mediscreen/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package mediscreen.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
